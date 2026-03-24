07b86192cc2b1fa5efebafbfe8f8fd3d27226545
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-24 19:07:38 +0530

    Added area demo project

---
+++ AreaDemo.java
@@ -0,0 +1,26 @@
+package com.ilp.tcs;
+
+import java.util.Scanner;
+
+public class AreaDemo {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter plot length: ");
+		double plotLength = sc.nextDouble();
+		System.out.println("Enter plot width: ");
+		double plotWidth = sc.nextDouble();
+		System.out.println("Enter Construction length: ");
+		double constLength = sc.nextDouble();
+		System.out.println("Enter Construction Width: ");
+		double constWidth = sc.nextDouble();
+		double openArea = calculateArea(plotLength,  plotWidth,  constLength,  constWidth);
+		System.out.println("OpenArea is : "+ openArea);
+		
+	}
+	
+	public static double calculateArea(double plotLength, double plotWidth, double constLength, double constWidth) {
+		double openArea = 0;
+		openArea = plotLength*plotWidth-constLength*constWidth;
+		return openArea;
+	}
+}
