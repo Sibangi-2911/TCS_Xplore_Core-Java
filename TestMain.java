ff9cbf24344a5dfc3c5246ed71b655ab1f536910
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-28 13:10:40 +0530

    Implementation of this keyword

---
+++ TestMain.java
@@ -0,0 +1,11 @@
+package com.ilp.tcs;
+
+public class TestMain {
+	public static void main(String args[]) {
+		Employee1 e1 = new Employee1(101, "Sibangi");
+		Employee1 e2 = new Employee1(102, "Seetal");
+		
+		e1.display();
+		e2.display();	
+	}
+}
